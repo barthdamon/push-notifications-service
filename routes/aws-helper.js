5142666853e99cba08557b01d9ab7301bffffa8e
MattxDamon <mbmattbarth@gmail.com>
2016-03-31 17:08:56 -0400

    promisifies device registration and adds it to awshelper

--- routes/aws-helper.js
+++ routes/aws-helper.js
@@ -13,3 +13,32 @@ exports.publishToSNS = function (params, sns) {
 		.done();
 	});
 };
+
+exports.registerDeviceWithSNS = function (topicArn, applicationArn, deviceToken, sns) {
+	const endpointParams = {
+		PlatformApplicationArn: applicationArn, /* required */
+		Token: deviceToken /* required */
+	};
+
+	const newEndpoint = Promise.promisify(sns.createPlatformEndpoint);
+	const subscribe = Promise.promisify(sns.subscribe);
+
+	return new Promise((resolve, reject) => {
+		return newEndpoint(endpointParams)
+			.then(data => {
+				const subscriptionParams = {
+					Protocol: 'application', /* required */
+					TopicArn: topicArn, /* required */
+					Endpoint: data.EndpointArn /* required */
+				};
+				return subscribe(subscriptionParams) //eslint-disable-line
+			})
+			.then(data => {
+				return resolve(data);
+			})
+			.catch(err => {
+				return reject(err);
+			})
+		.done();
+	});
+};
--- routes/device-tokens.js
+++ routes/device-tokens.js
@@ -1,34 +1,44 @@
+const Promise = require('bluebird');
+const AWSHelper = require('./aws-helper.js');
+
 exports.createPlatformEndpoint = (topicArn, applicationArn, deviceToken, sns) => {
+	console.log(`Registering Device: ${deviceToken}, application: ${applicationArn}, topicArn: ${topicArn}`);
 	return new Promise((resolve, reject) => {
-		const params = {
-			PlatformApplicationArn: applicationArn, /* required */
-			Token: deviceToken /* required */
-		};
-		sns.createPlatformEndpoint(params, function(err, data) { // eslint-disable-line
-			if (err) {
-				console.log('new platform endpoint creation failure');
-				console.log(err);
-				reject(err);
-			} else {
-				console.log('new platform endpoint creation success');
-				console.log(data);
-				const params = {
-					Protocol: 'application', /* required */
-					TopicArn: topicArn, /* required */
-					Endpoint: data.EndpointArn /* required */
-				};
-				sns.subscribe(params, function (err, data) { // eslint-disable-line
-					if (err) {
-						console.log('Subscription failed');
-						console.log(err); // an error occurred
-						reject(err);
-					} else {
-						console.log('Subscription succeeded');
-						console.log(data);
-						resolve(data);
-					}
-				});
-			}
-		});
+		AWSHelper.registerDeviceWithSNS(topicArn, applicationArn, deviceToken, sns)
+			.then(data => {
+				return resolve(data);
+			})
+			.catch(err => {
+				return reject(err);
+			})
+		.done();
 	});
 };
+// 		sns.createPlatformEndpoint(params, function(err, data) { // eslint-disable-line
+// 			if (err) {
+// 				console.log('new platform endpoint creation failure');
+// 				console.log(err);
+// 				reject(err);
+// 			} else {
+// 				console.log('new platform endpoint creation success');
+// 				console.log(data);
+// 				const params = {
+// 					Protocol: 'application', /* required */
+// 					TopicArn: topicArn,  required  //eslint-disable-line
+// 					Endpoint: data.EndpointArn /* required */
+// 				};
+// 				sns.subscribe(params, function (err, data) { // eslint-disable-line
+// 					if (err) {
+// 						console.log('Subscription failed');
+// 						console.log(err); // an error occurred
+// 						reject(err);
+// 					} else {
+// 						console.log('Subscription succeeded');
+// 						console.log(data);
+// 						resolve(data);
+// 					}
+// 				});
+// 			}
+// 		});
+// 	});
+// };

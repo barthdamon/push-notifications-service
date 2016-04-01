5a54bffbf35c2d786ca4d1502327c44105099eeb
MattxDamon <mbmattbarth@gmail.com>
2016-04-01 12:01:55 -0400

    callback aws for testing

--- routes/device-tokens.js
+++ routes/device-tokens.js
@@ -2,17 +2,48 @@ const Promise = require('bluebird');
 const AWSHelper = require('./aws-helper.js'); // eslint-disable-line
 
 exports.createPlatformEndpoint = (topicArn, applicationArn, deviceToken, sns) => { // eslint-disable-line
-	console.log(`Pushem registering Device: ${deviceToken}, application: ${applicationArn}, topicArn: ${topicArn}`);
+	console.log(`Device Token registering Device: ${deviceToken}, application: ${applicationArn}, topicArn: ${topicArn}`);
 	return new Promise((resolve, reject) => { // eslint-disable-line
 		// resolve(true);
-		return AWSHelper.registerDeviceWithSNS(topicArn, applicationArn, deviceToken, sns)
-			.then(data => {
-				return resolve(data);
-			})
-			.catch(err => {
-				return reject(err);
-			})
-		.done();
+		// return AWSHelper.registerDeviceWithSNS(topicArn, applicationArn, deviceToken, sns)
+		// 	.then(data => {
+		// 		return resolve(data);
+		// 	})
+		// 	.catch(err => {
+		// 		return reject(err);
+		// 	})
+		// .done();
+
+		const endpointParams = {
+			PlatformApplicationArn: applicationArn, /* required */
+			Token: deviceToken /* required */
+		};
+		sns.createPlatformEndpoint(endpointParams, function(err, data) { // eslint-disable-line
+			if (err) {
+				console.log('new platform endpoint creation failure');
+				console.log(err);
+				reject(err);
+			} else {
+				console.log('new platform endpoint creation success');
+				console.log(data);
+				const params = {
+					Protocol: 'application', /* required */
+					TopicArn: topicArn,  //eslint-disable-line
+					Endpoint: data.EndpointArn /* required */
+				};
+				sns.subscribe(params, function (err, data) { // eslint-disable-line
+					if (err) {
+						console.log('Subscription failed');
+						console.log(err); // an error occurred
+						reject(err);
+					} else {
+						console.log('Subscription succeeded');
+						console.log(data);
+						resolve(data);
+					}
+				});
+			}
+		});
 	});
 };
 // 		sns.createPlatformEndpoint(params, function(err, data) { // eslint-disable-line

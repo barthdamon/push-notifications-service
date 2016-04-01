432df5a12c3b596ed00e2cf3e1a87bb5dd5a200d
MattxDamon <mbmattbarth@gmail.com>
2016-04-01 15:41:47 -0400

    stringify none of the things

--- routes/pushem.js
+++ routes/pushem.js
@@ -23,7 +23,7 @@ exports.sendNotification = (topicArn, message, sns) => {
 		const snsParams = {
 			TopicArn: topicArn,
 			MessageStructure: 'json',
-			Message: JSON.stringify(finalMessage)
+			Message: finalMessage
 		};
 		AWSHelper.publishToSNS(snsParams, sns)
 			.then(data => {
@@ -42,10 +42,10 @@ function buildApplePush(appleMessage, appleLink) {
 		return null;
 	}
 
-	return JSON.stringify({
+	return {
 		aps: {alert: appleMessage},
 		link: (appleLink || 'collectionIdHere')
-	});
+	};
 }
 
 function buildAndroidPush(androidMessage, androidLink, title) {
@@ -60,6 +60,6 @@ function buildAndroidPush(androidMessage, androidLink, title) {
 	if (title) {
 		result.data.title = title;
 	}
-	return JSON.stringify(result);
+	return result;
 }
 
